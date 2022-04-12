8c46fe784c05cc1a0c10f13c53b9cefd76ff30a3
Jack <jackz163299@gmail.com>
2022-04-12 17:35:32 -0400

    add comments

--- contracts/SplitPayment.sol
+++ contracts/SplitPayment.sol
@@ -5,16 +5,19 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-// import "hardhat/console.sol";
-
+/// @notice SplitPayment is a contract that allows to split Ether & ERC20 payments among a list of accounts
 contract SplitPayment is Ownable {
     using SafeERC20 for IERC20;
 
+    /// @notice list of accounts
     mapping(address => uint) public accounts;
     uint public accountLen;
-    // uint 
+    
+    /// @notice eth pools
     mapping (uint => uint) public ethPools;
     uint public ethPoolLen;
+
+    /// @notice erc20 token pools
     mapping (uint => mapping(address => uint)) public tokenPools;
     uint public tokenPoolLen;
 
@@ -28,9 +31,14 @@ contract SplitPayment is Ownable {
     }
 
     receive () external payable {
-        
+        deposit();
     }
 
+    /**
+     * @notice update accounts with shares
+     * @param _accounts:address[] list of accounts
+     * @param _shares:uint[] list of shares 
+     */
     function updateAccounts(address[] memory _accounts, uint[] memory _shares) public onlyOwner {
         uint _accountLen = _accounts.length;
         uint _shareLen = _shares.length;
@@ -51,17 +59,27 @@ contract SplitPayment is Ownable {
         accountLen = _accountLen;
     }
 
+    /// @notice deposit eth
     function deposit() public payable {
         ethPools[ethPoolLen] = msg.value;
         emit Deposit(ethPoolLen ++, msg.sender, msg.value);
     }
 
+    /**
+     * @notice withdraw eth of specific pool
+     * @param pid:uint eth pool id 
+     */
     function withdraw(uint pid) external {
         uint amount = ethPools[pid] * accounts[msg.sender] / 1e8;
         payable(msg.sender).transfer(amount);
         emit Withdraw(pid, msg.sender, amount);
     }
 
+    /**
+     * @notice deposit token 
+     * @param token:address token address
+     * @param amount:uint amount of token
+     */ 
     function depositToken(address token, uint amount) external {
         tokenPools[tokenPoolLen ++][token] = amount;
         IERC20(token).safeTransferFrom(msg.sender, address(this), amount);
@@ -69,6 +87,10 @@ contract SplitPayment is Ownable {
         emit DepositToken(tokenPoolLen ++, msg.sender, token, amount);
     }
 
+    /** @notice withdraw token of specific pool
+     * @param pid:uint token pool id
+     * @param token:address token address
+     */
     function withdrawToken(uint pid, address token) external {
         uint amount = tokenPools[pid][token] * accounts[msg.sender] / 1e8;
         IERC20(token).safeTransfer(msg.sender, amount);
